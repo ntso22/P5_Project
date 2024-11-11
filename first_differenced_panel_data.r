d80845f9baeb9912c14351f0eaecd971ab41d9f1
ntso22 <159235666+ntso22@users.noreply.github.com>
2024-11-11 10:56:44 +0100

    Update first_differenced_panel_data.r
    removed useless column of dataframe

--- first_differenced_panel_data.r
+++ first_differenced_panel_data.r
@@ -6,4 +6,5 @@ panel_data %>%
     mutate(Areal_Bolig = Areal_Bolig - lag(Areal_Bolig), Salgstid = Salgstid - lag(Salgstid), Pris_Salg = Pris_Salg - lag(Pris_Salg), Areal_Grund = Areal_Grund - lag(Areal_Grund), StorGrund = StorGrund - lag(StorGrund), Dist_skole = Dist_skole - lag(Dist_skole), Dist_raadhus = Dist_raadhus - lag(Dist_raadhus), Alder = Alder - lag(Alder), AntalFremvisninger - lag(AntalFremvisninger), Areal_GarageCarport = Areal_GarageCarport - lag(Areal_GarageCarport), Ejd_AntalPlan = Ejd_AntalPlan - lag(Ejd_AntalPlan)) %>%
     arrange(KommuneNavn) %>%
     subset(Salgsaar != 2011) %>%
+    select(-X) %>%
     write.csv("first_differenced_panel_data.csv")
