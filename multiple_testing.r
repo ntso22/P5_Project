0650cdf057f26fd329c5f69b83faaa42ff04a278
ntso22 <159235666+ntso22@users.noreply.github.com>
2024-11-20 11:02:14 +0100

    Add files via upload
    Adding script for Benjamini-Yekutieli procedure along with csv file containing information about the test performed on the initial model

---
+++ multiple_testing.r
@@ -0,0 +1,37 @@
+main <- function() {
+    library(dplyr)
+
+    # Setting up data frame properly
+    data <- read.csv("linear_model_1.csv")
+    names <- data$X
+    rownames(data) <- names
+    data <- data[-1]
+    colnames(data)[4] <- "p_value"
+
+    # Ordering data frame with respect to p-values to prepare for Benjamini-Yekutieli procedure
+    data <- arrange(data, p_value)
+
+    # Benjamini-Yekutieli procedure
+    m <- nrow(data)
+    alpha <- .05
+    k <- 0
+    harmonic <- 0
+    for (l in 1:m) {
+        harmonic <- harmonic + 1/l
+    }
+    for (i in 1:m) {
+        alpha_i <- i * alpha / (m * harmonic)
+        if (data$p_value[i] <= alpha_i) {
+            k <- i
+        }
+    }
+
+    # Creating column describing whether or not the null hypothesis is rejected and writing the result to csv
+    is_rejected <- append(rep('Rejected', k), rep('Not rejected', m - k))
+    data <- mutate(data, BY_test_result = is_rejected)
+    write.csv(data, 'Benjamini_Yekutieli.csv')
+
+    return(data)
+}
+
+main()
