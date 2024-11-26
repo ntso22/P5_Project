aba96f309fef05d3fa1dccf8383a6b2eefcd0754
ntso22 <159235666+ntso22@users.noreply.github.com>
2024-11-26 14:25:54 +0100

    Update pooled_crosssectional.r

--- pooled_crosssectional.r
+++ pooled_crosssectional.r
@@ -30,7 +30,7 @@ main <- function() {
     DataSet <- homedata %>%
         select(vars) %>%
         na.omit() %>%
-        mutate(Salgsaar = as.character(Salgsaar)) %>%
+        mutate(Salgsaar = as.character(Salgsaar), Pris_Salg = log(Pris_Salg)) %>%
         subset(KommuneNavn %in% cities)
 
     # Possibly need to combine Salgsaar and KommuneNavn as well
@@ -47,6 +47,27 @@ main <- function() {
         data = DataSet)
 
     summary <- summary(model)
+
+    DataSet <- DataSet %>% mutate(p_value = 2 * (1 - pnorm(abs(model$residuals / summary$sigma)))) %>%
+        arrange(p_value)
+    # Benjamini-Yekutieli procedure
+    m <- nrow(DataSet)
+    alpha <- .05
+    k <- 0
+    harmonic <- sum(1 / (1:m))
+    for (i in 1:m) {
+        alpha_i <- i * alpha / (m * harmonic)
+        if (DataSet$p_value[i] <= alpha_i) {
+            k <- i
+        }
+    }
+    DataSet <- DataSet[-(1:k),]
+    print(k)
+
     write.csv(summary$coefficients, 'linear_model_1.csv')
-    return (summary)
+
+    str(DataSet)
+    #return (summary)
 }
+
+main()
